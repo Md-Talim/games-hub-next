8c6f96b23a628249df0ca5716ce47107f9ce9631
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-22 20:08:14 +0530

    add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import logo from './logo.webp';
+import meh from './meh.webp';
+import noImage from './no-image-placeholder.webp';
+import like from './thumbs-up.webp';
+import bullsEye from './bulls-eye.webp';
+
+export { logo, meh, noImage, like, bullsEye };
