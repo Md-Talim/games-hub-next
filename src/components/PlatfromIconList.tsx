51eff4839f1f661f0cd09614fdd0d4d54c4e1fac
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-23 17:02:39 +0530

    Add platform icons

---
+++ src/components/PlatfromIconList.tsx
@@ -0,0 +1,42 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGame';
+import { IconType } from 'react-icons';
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatfromIconList = ({ platforms = [] }: Props) => {
+  const icons: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={2}>
+      {platforms.map(({ id, slug }) => (
+        <Icon key={id} as={icons[slug]} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatfromIconList;
