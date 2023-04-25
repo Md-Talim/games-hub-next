946254229f42a5f9229b7bb2b7ca0f675d7acfd2
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-25 07:27:02 +0530

    Add a platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,11 +16,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={4}
-        paddingX={5}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={4}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
