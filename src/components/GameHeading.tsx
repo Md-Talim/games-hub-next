5a50baa1b0279eda48977cc37aa86a514082aaed
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-28 10:32:08 +0530

    Make the font size small on smaller devices

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -10,7 +10,11 @@ const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
   return (
-    <Heading as='h1' marginY={5} fontSize='5xl'>
+    <Heading
+      as='h1'
+      marginY={5}
+      fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}
+    >
       {heading}
     </Heading>
   );
