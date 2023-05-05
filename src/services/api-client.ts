ca3a16a6b24d511b7d35fda1b2175f912a3db99e
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-05 09:35:02 +0530

    Refactor: Fetching games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+export interface fetchDataResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   params: {
