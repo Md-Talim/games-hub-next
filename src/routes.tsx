c77a5223f70291e16c33f47e82898248120253da
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-18 08:15:52 +0530

    Handling Errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './pages/Layout';
 import Home from './pages/Home';
+import Error from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <Error />,
     children: [{ index: true, element: <Home /> }],
   },
 ]);
