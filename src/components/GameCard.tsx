dc44d4ef5e65a8245935f1b86e60b7c535e4994c
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-18 22:38:04 +0530

    Fetching game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import getOptimizedImage from '../services/image-url';
 import PlatfromIconList from './PlatfromIconList';
 import CriticScore from './CriticScore';
 import Emoji from './Emoji';
+import { Link } from 'react-router-dom';
 
 interface Props {
   game: Game;
@@ -23,7 +24,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize='2xl' marginBottom={2}>
-          {game.name}
+          <Link to={'/games/' + game.slug}>{game.name}</Link>
         </Heading>
         <Emoji rating={game.rating_top} />
       </CardBody>
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './pages/Layout';
 import Home from './pages/Home';
 import Error from './pages/Error';
+import GameDetail from './pages/GameDetails';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <Error />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'games/:slug', element: <GameDetail /> },
+    ],
   },
 ]);
 
