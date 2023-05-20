944facb863422815bee0f201b38f2ed1edc0d714
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-20 21:47:21 +0530

    Refactor: Extract game attributes in a component

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -1,6 +1,7 @@
-import { Box, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import { Box, Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import { CriticScore, DefinitionItem, ExpandableText } from '../components';
+import { ExpandableText } from '../components';
+import GameAttributes from '../components/GameAttributes';
 import useGame from '../hooks/useGame';
 
 const GameDetail = () => {
@@ -20,26 +21,7 @@ const GameDetail = () => {
     <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText text={game.description} />
-      <SimpleGrid columns={2} maxWidth='5xl' marginX='auto'>
-        <DefinitionItem term='Platforms'>
-          {game.parent_platforms.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term='Publishers'>
-          {game.publishers.map((publisher) => (
-            <Text key={publisher.id}>{publisher.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term='Genres'>
-          {game.genres.map((genre) => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term='Metacritic'>
-          <CriticScore score={game.metacritic} />
-        </DefinitionItem>
-      </SimpleGrid>
+      <GameAttributes game={game} />
     </Box>
   );
 };
