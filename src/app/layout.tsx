72faf0df97e7fbb84c4e7e4c1be75cfe396241f2
Mohammad Talim <talimmohammad116@gmail.com>
2024-05-11 18:39:40 +0530

    create the layout for the homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/navbar";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={(clsx(inter.className), "max-h-screen")}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
