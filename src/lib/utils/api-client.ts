57461df3156cd81412691caebfb034c72d2961ca
Mohammad Talim <talimmohammad116@gmail.com>
2024-11-11 06:44:26 +0530

    feat: implement caching

--- src/lib/utils/api-client.ts
+++ src/lib/utils/api-client.ts
@@ -15,21 +15,37 @@ const axiosInstance = axios.create({
 
 class APIClient<T> {
   endpoint: string;
+  cache: Map<string, T | FetchDataResponse<T>> = new Map();
 
   constructor(endpoint: string) {
     this.endpoint = endpoint;
   }
 
   getAll = async (config?: AxiosRequestConfig) => {
+    const cacheKey = `${this.endpoint}_all`;
+
+    if (this.cache.has(cacheKey)) {
+      return this.cache.get(cacheKey) as FetchDataResponse<T>;
+    }
+
     const res = await axiosInstance.get<FetchDataResponse<T>>(
       this.endpoint,
       config
     );
+    this.cache.set(cacheKey, res.data);
     return res.data;
   };
 
   get = async (id: number | string) => {
+    const cacheKey = `${this.endpoint}_${id}`;
+
+    if (this.cache.has(cacheKey)) {
+      return this.cache.get(cacheKey) as T;
+    }
+
     const res = await axiosInstance.get<T>(this.endpoint + "/" + id);
+    this.cache.set(cacheKey, res.data);
+
     return res.data;
   };
 }
