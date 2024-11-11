916b7868c85afba161ebc48c8214b09278f01217
Mohammad Talim <talimmohammad116@gmail.com>
2024-11-11 06:31:15 +0530

    fix: navigate to search results on game details page

--- src/components/navbar/search-input.tsx
+++ src/components/navbar/search-input.tsx
@@ -1,39 +1,34 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useRef } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
 const SearchInput = () => {
-  const ref = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState("");
   const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { push, replace } = useRouter();
+  const router = useRouter();
 
   const updateParams = (text: string) => {
     const params = new URLSearchParams(searchParams);
     params.set("search", text);
-
-    replace(`${pathname}?${params.toString()}`);
+    router.push(`/?${params.toString()}`);
   };
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-
-        if (ref.current) {
-          push("/");
-          updateParams(ref.current.value);
-        }
+        updateParams(searchValue);
       }}
     >
       <div className="w-52 md:w-96 flex items-center gap-3 py-2 px-4 bg-licorice rounded-md border border-codGray">
         <BsSearch className="h-5 w-5 text-mountainMist" />
         <input
-          ref={ref}
           type="text"
-          placeholder="Searh game..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Search game..."
           className="flex-1 text-lg bg-transparent text-springBud outline-none"
         />
       </div>
